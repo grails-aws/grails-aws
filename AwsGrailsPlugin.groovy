25ca9f96dba3673797690061d92081a5efce7bd9
lucastex <lucastex@gmail.com>
2011-01-17 22:08:20 -0200

    changed plugin version to 0.2

--- AwsGrailsPlugin.groovy
+++ AwsGrailsPlugin.groovy
@@ -4,7 +4,7 @@ import org.codehaus.groovy.grails.commons.ConfigurationHolder
 class AwsGrailsPlugin {
 	
     // the plugin version
-    def version = "0.1"
+    def version = "0.2"
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.0 > *"
