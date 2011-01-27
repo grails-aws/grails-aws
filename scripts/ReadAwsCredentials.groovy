c9f2690bbd3e28743b3ea41f106eb4f43ca417af
lucastex <lucastex@gmail.com>
2011-01-27 14:15:32 -0200

    refactor for reading AWS Credentials

--- scripts/ReadAwsCredentials.groovy
+++ scripts/ReadAwsCredentials.groovy
@@ -6,32 +6,25 @@ import com.amazonaws.auth.PropertiesCredentials
 includeTargets << grailsScript("_GrailsPackage")
 
 target (readAwsCredentials: "Reads user AWS Credentials from Config.groovy") {
-
-	depends(createConfig)
 	
-	println config
-	
-	//def config = new ConfigSlurper().parse(new File('grails-app/conf/Config.groovy').toURL())
+	def config = new ConfigSlurper().parse(new File('grails-app/conf/Config.groovy').toURL())
 	def accessKey  = config.grails?.plugin?.aws?.credentials?.accessKey
 	def secretKey  = config.grails?.plugin?.aws?.credentials?.secretKey
 	def properties = config.grails?.plugin?.aws?.credentials?.properties
 	
-	def awsCredentials 
-	
 	//load from properties, prefered
 	if (properties) {
-		awsCredentials = new PropertiesCredentials(new File(properties))
-	} else {
-		
-		//plain text config
-		if (accessKey && secretKey) {
-			awsCredentials = new BasicAWSCredentials(accessKey, secretKey)
-		} else {
-			throw new Exception("Please check user guide to see how you should configure AWS credentials")
-		}
-	}	
+		def awsCredentials = new PropertiesCredentials(new File(properties))
+		accessKey = awsCredentials.getAWSAccessKeyId()
+		secretKey = awsCredentials.getAWSSecretKey()
+	} 
+	
+	if (!accessKey || !secretKey) {
+		println "[AWS ERROR] Please check user guide to see how you should configure AWS credentials"
+		System.exit(1)
+	}
 
-	ant.property ( name: "accessKey", value: awsCredentials.getAWSAccessKeyId() )
-	ant.property ( name: "secretKey", value: awsCredentials.getAWSSecretKey() )
+	ant.property ( name: "accessKey", value: accessKey )
+	ant.property ( name: "secretKey", value: secretKey )
 
 }
