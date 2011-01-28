eee34d269f481654a7d24cd2185566dec44b2f65
lucastex <lucastex@gmail.com>
2011-01-28 15:42:35 -0200

    controller to test SES sending

--- grails-app/controllers/aws/ses/SesTestController.groovy
+++ grails-app/controllers/aws/ses/SesTestController.groovy
@@ -5,12 +5,71 @@ class SesTestController {
     def sendPlainTextMail = { 
 	
 		def mailId = sesMail {
-			from "lucastex@gmail.com"
 			to "lucastex@gmail.com"
 			subject "test plain text mail"
-			body "this is a plain text (${new Date().format('dd/MM/yyyy HH:mm')})"
+			body "sendPlainTextMail (${new Date().format('dd/MM/yyyy HH:mm')})"
 		}
 		
 		render "E-mail sent: ${mailId}"
 	}
+
+    def sendPlainTextMultipleTo = { 
+	
+		def mailId = sesMail {
+			to "lucastex@gmail.com", "l.ucastex@gmail.com", "lu.castex@gmail.com"
+			subject "test plain text mail"
+			body "sendPlainTextMultipleTo (${new Date().format('dd/MM/yyyy HH:mm')})"
+		}
+		
+		render "E-mail sent: ${mailId}"
+	}
+	
+	def sendPlainTextMailCC = { 
+	
+		def mailId = sesMail {
+			to "lucastex@gmail.com"
+			cc "l.ucastex@gmail.com"
+			subject "test plain text mail"
+			body "sendPlainTextMailCC (${new Date().format('dd/MM/yyyy HH:mm')})"
+		}
+		
+		render "E-mail sent: ${mailId}"
+	}
+
+	def sendPlainTextMailMultipleCC = { 
+	
+		def mailId = sesMail {
+			to "lucastex@gmail.com"
+			cc "l.ucastex@gmail.com", "lu.castex@gmail.com"			
+			subject "test plain text mail"
+			body "sendPlainTextMailMultipleCC (${new Date().format('dd/MM/yyyy HH:mm')})"
+		}
+		
+		render "E-mail sent: ${mailId}"
+	}
+
+	def sendPlainTextMailBCC = { 
+	
+		def mailId = sesMail {
+			to "lucastex@gmail.com"
+			bcc "l.ucastex@gmail.com"
+			subject "test plain text mail"
+			body "sendPlainTextMailBCC (${new Date().format('dd/MM/yyyy HH:mm')})"
+		}
+		
+		render "E-mail sent: ${mailId}"
+	}
+
+	def sendPlainTextMailMultipleBCC = { 
+	
+		def mailId = sesMail {
+			to "lucastex@gmail.com"
+			bcc "l.ucastex@gmail.com", "lu.castex@gmail.com"			
+			subject "test plain text mail"
+			body "sendPlainTextMailMultipleBCC (${new Date().format('dd/MM/yyyy HH:mm')})"
+		}
+		
+		render "E-mail sent: ${mailId}"
+	}
+    
 }
