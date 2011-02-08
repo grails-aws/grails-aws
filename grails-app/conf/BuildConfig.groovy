a6eda86e8131b8e7e58e13eb3261b10c8f5a7c60
lucastex <lucastex@gmail.com>
2011-02-08 14:33:39 -0200

    BuildConfig updated for improved dependency resolution

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -9,15 +9,21 @@ grails.project.dependency.resolution = {
 
     log "warn" 
 
-    repositories {
-        grailsPlugins()
+    repositories {	
+		mavenCentral()
+		grailsPlugins()
         grailsHome()
         grailsCentral()
-		mavenCentral()
     }
 
-    dependencies {	
-		compile 'net.java.dev.jets3t:jets3t:0.8.0'
-    }
-
+	dependencies {
+        compile 'net.java.dev.jets3t:jets3t:0.8.0'
+        compile 'javax.mail:mail:1.4.1'
+        compile 'commons-httpclient:commons-httpclient:3.1'
+        compile 'commons-logging:commons-logging:1.1.1'
+		compile 'org.codehaus.jackson:jackson-core-asl:1.7.2'
+		compile 'com.amazonaws:aws-java-sdk:1.1.5', {
+			excludes 'stax-api', 'jackson-core-asl', 'commons-httpclient', 'commons-logging'
+		}
+     }
 }
