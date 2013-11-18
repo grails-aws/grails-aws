ea03e21db9e46f4e55ce6dfdf783b5b99fce9467
Burt Beckwith <burt@burtbeckwith.com>
2013-11-18 13:05:52 -0500

    Grails 2.0+ is required to release plugins

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,35 +1,30 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = 'target'
 
 grails.project.dependency.resolution = {
 
-    inherits("global") {
-    }
+    inherits 'global'
+    log 'warn'
 
-    log "warn" 
-
-    repositories {	
-		mavenCentral()
-		grailsPlugins()
-        grailsHome()
+    repositories {
         grailsCentral()
+        mavenLocal()
+        mavenCentral()
     }
 
-	dependencies {
+    dependencies {
         compile 'net.java.dev.jets3t:jets3t:0.9.0'
         compile 'javax.mail:mail:1.4.1'
         compile 'commons-httpclient:commons-httpclient:3.1'
         compile 'commons-logging:commons-logging:1.1.1'
-		compile 'org.codehaus.jackson:jackson-core-asl:1.7.2'
-		compile 'com.amazonaws:aws-java-sdk:1.2.12', {
-			excludes 'stax-api', 'jackson-core-asl', 'commons-httpclient', 'commons-logging'
-		}
-     }
+        compile 'org.codehaus.jackson:jackson-core-asl:1.7.2'
+        compile 'com.amazonaws:aws-java-sdk:1.2.12', {
+            excludes 'stax-api', 'jackson-core-asl', 'commons-httpclient', 'commons-logging'
+        }
+    }
 
-	plugins {
-		
-		build ':release:1.0.1', { export = false }
-        
-	}
+    plugins {
+        build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+            export = false
+        }
+    }
 }
