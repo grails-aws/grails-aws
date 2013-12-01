eaa67ecca6186595bf8c8a0f0c288cbebca60a63
Jay Prall <jay.prall@affectiva.com>
2013-12-01 14:59:18 -0500

    remove mavenLocal() from repositories and remove commons-httpclient 3.1 because jets3t depends on 4.1.2

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -7,18 +7,19 @@ grails.project.dependency.resolution = {
 
     repositories {
         grailsCentral()
-        mavenLocal()
         mavenCentral()
     }
 
     dependencies {
+
         compile 'net.java.dev.jets3t:jets3t:0.9.0'
+        // jetset 0.9.0 requires httpclient 4.1.2
+
         compile 'javax.mail:mail:1.4.1'
-        compile 'commons-httpclient:commons-httpclient:3.1'
         compile 'commons-logging:commons-logging:1.1.1'
         compile 'org.codehaus.jackson:jackson-core-asl:1.7.2'
         compile 'com.amazonaws:aws-java-sdk:1.2.12', {
-            excludes 'stax-api', 'jackson-core-asl', 'commons-httpclient', 'commons-logging'
+            excludes 'stax-api', 'jackson-core-asl', 'httpclient', 'commons-logging'
         }
     }
 
