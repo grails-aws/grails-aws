5cd06d73786fbd76332bfc9aeb5d5e3f35c2e10a
Jay Prall <jay.prall@affectiva.com>
2013-12-15 08:06:09 -0500

    restore integration test that throws

--- test/integration/grails/aws/S3IntegrationTests.groovy
+++ test/integration/grails/aws/S3IntegrationTests.groovy
@@ -7,16 +7,11 @@ class S3IntegrationTests extends GroovyTestCase {
 
     def aws
 
-	void testSomething() {
-		assert true
-	}
-
-	// TODO: get this working in grails 2.3.4
-    // void testGetOnAMissingFileThrowsS3ServiceException() {
-    // 
-    //     def message = shouldFail(S3ServiceException) {
-    //         aws.s3().on('missing-bucket').get('missing-file.txt')
-    //     }
-    //     assert message == "S3 Error Message."
-    // }
+    void testGetOnAMissingFileThrowsS3ServiceException() {
+    
+        def message = shouldFail(S3ServiceException) {
+            aws.s3().on('missing-bucket').get('missing-file.txt')
+        }
+        assert message == "S3 Error Message."
+    }
 }
