5a42a9813bb484b465a7c935505883cef2c0bde4
Jay Prall <jay.prall@affectiva.com>
2013-12-17 10:13:35 -0500

    add simple integration test for AWSGenericTools

---
+++ test/integration/grails/plugin/aws/AWSGenericToolsIntegrationTests.groovy
@@ -0,0 +1,16 @@
+package grails.plugin.aws
+
+import grails.plugin.aws.s3.AWSS3Tools
+import static org.junit.Assert.*
+import org.junit.*
+
+class AWSGenericToolsIntegrationTests {
+
+	def aws
+
+    @Test
+    void s3MethodReturnsHelperToTheS3Service() {
+		
+		assert aws.s3().class == AWSS3Tools
+    }
+}
