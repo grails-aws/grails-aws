6938e69ec11a58bcca84af7a6a6a5bae037e3b6d
Jay Prall <jay.prall@affectiva.com>
2013-12-19 17:48:19 -0500

    clean hard tabs

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,8 +1,8 @@
 log4j = {
-	error 'org.codehaus.groovy.grails',
-	      'org.springframework',
-	      'org.hibernate',
-	      'net.sf.ehcache.hibernate'
+    error 'org.codehaus.groovy.grails',
+          'org.springframework',
+          'org.hibernate',
+          'net.sf.ehcache.hibernate'
 }
 
 grails.doc.authors = 'Lucas Teixeira, Jay Prall'
@@ -11,21 +11,21 @@ grails.doc.title = 'Grails AWS Plugin'
 
 // These settings are only used for plugin development
 grails {
-	plugin {
-		aws {
-			credentials {
-				accessKey = "fake-access-key"
-				secretKey = "fake-secret-key"
-			}
-			s3 {
-				bucket = "grails-aws-test-bucket-20110515"
-				rrs    = true
-				acl    = "public"
-			}
-			ses {
-				from     = "lucastex@gmail.com"
-				//enabled  = false
-			}
-		}
-	}
+    plugin {
+        aws {
+            credentials {
+                accessKey = "fake-access-key"
+                secretKey = "fake-secret-key"
+            }
+            s3 {
+                bucket = "grails-aws-test-bucket-20110515"
+                rrs    = true
+                acl    = "public"
+            }
+            ses {
+                from     = "lucastex@gmail.com"
+                //enabled  = false
+            }
+        }
+    }
 }
