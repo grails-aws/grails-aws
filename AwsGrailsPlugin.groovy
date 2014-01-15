bc146efcfec7f98a3ccd3676d5c1cd0cbb17aa2a
Jay Prall <jay.prall@affectiva.com>
2014-01-15 10:51:38 -0500

    release 1.6.7.3

--- AwsGrailsPlugin.groovy
+++ AwsGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import grails.plugin.aws.meta.AwsPluginSupport
 
 class AwsGrailsPlugin {
 
-    def version                  = "1.6.7.2"
+    def version                  = "1.6.7.3"
     def grailsVersion            = "1.3.0 > *"
     def loadAfter                = ['services', 'controllers']
     def observe                  = ['services', 'controllers']
