2cd87366aec82cd0705a91f6a60abf4a861796fb
Jay Prall <jay.prall@affectiva.com>
2015-01-05 12:40:20 -0500

    update to use aws-java-sdk 1.9.13

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -16,8 +16,8 @@ grails.project.dependency.resolution = {
     dependencies {
 
         compile 'net.java.dev.jets3t:jets3t:0.9.0'
-        // jetset 0.9.0 requires httpclient 4.1.2
-		
+        // jetset 0.9.0 requires httpclient > 4.1.2
+
         build 'org.apache.httpcomponents:httpcore:4.2'
         build 'org.apache.httpcomponents:httpclient:4.2'
         runtime 'org.apache.httpcomponents:httpcore:4.2'
@@ -27,7 +27,8 @@ grails.project.dependency.resolution = {
         compile 'javax.mail:mail:1.4.1'
         compile 'commons-logging:commons-logging:1.1.1'
         compile 'org.codehaus.jackson:jackson-core-asl:1.7.2'
-        compile 'com.amazonaws:aws-java-sdk:1.7.5', {
+
+        compile 'com.amazonaws:aws-java-sdk:1.9.13', {
             excludes 'stax-api', 'jackson-core-asl', 'httpclient', 'commons-logging'
         }
     }
