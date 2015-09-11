0d5a8a5e455931a4cfdc974bf239c4e78e61125a
Jay Prall <jay.prall@affectiva.com>
2015-09-11 13:07:35 -0400

    update to use httpclient 4.3.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -17,12 +17,12 @@ grails.project.dependency.resolution = {
     dependencies {
 
         compile 'net.java.dev.jets3t:jets3t:0.9.0'
-        // jetset 0.9.1 uses httpclient 4.3.2; httpcore 4.3.1
 
-        build 'org.apache.httpcomponents:httpcore:4.3.1'
-        build 'org.apache.httpcomponents:httpclient:4.3.2'
-        runtime 'org.apache.httpcomponents:httpcore:4.3.1'
-        runtime 'org.apache.httpcomponents:httpclient:4.3.2'
+        build 'org.apache.httpcomponents:httpclient:4.3.4'
+        runtime 'org.apache.httpcomponents:httpclient:4.3.4'
+        // httpcore 4.3.2 is a dependency of httpclient 4.3.4
+        build 'org.apache.httpcomponents:httpcore:4.3.2'
+        runtime 'org.apache.httpcomponents:httpcore:4.3.2'
 
         compile 'javax.mail:mail:1.4.1'
         compile 'commons-logging:commons-logging:1.1.1'
